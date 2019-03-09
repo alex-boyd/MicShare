4d381e95b70054f57465fbe11ba8b208ef2de97c
alex <alex.boyd737@gmail.com>
2019-03-09 13:27:10 -0800

    firebase set up

--- MicShare/AppDelegate.swift
+++ MicShare/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        let db = Firestore.firestore()
+
+        db.collection("cities").document("LA").setData([
+            "name": "Los Angeles"
+        ]) { (error:Error?) in
+            if let error = error {
+                print("\(error.localizedDescription)")
+            } else {
+                print("Document was successfully created and written.")
+            }
+         
+            
+            
+            
+        }
+        
         return true
     }
 
